5fb3226e7e9385f7fee939f7d4ee8740c0c61b97
imthiyazali <imthiyazali02@gmail.com>
2021-04-11 16:36:08 +0530

    This program will fetch new anime quotes from animechan api and add to the quotes file, then print any random quote twice without any space in between the line

--- get-quote.py
+++ get-quote.py
@@ -1,17 +1,33 @@
 import random
+import requests
 
 
 def primary():
     #print("Keep it logically awesome.")
 
-    f = open("quotes.txt")
+    f = open("quotes.txt", 'r+')
+
+    res = requests.get("https://animechan.vercel.app/api/random")
+    res = res.json()
+    newquote = res['quote']
+
+    # f.seek(0)
+    fdata = f.read()
+
+    if fdata:
+        f.write("\n")
+#
+    f.write(newquote)
+    f.seek(0)
     quotes = f.readlines()
+    # print(quotes)
     f.close()
 
-    last = 13
+    print(len(quotes))
+    last = len(quotes) - 1
     rnd = random.randint(0, last)
 
-    print(quotes[rnd])
+    print(quotes[rnd], quotes[rnd], sep="")
 
 
 if __name__ == "__main__":
